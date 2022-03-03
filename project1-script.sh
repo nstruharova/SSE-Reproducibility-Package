ac368a352fa879655fa0fe2f2524b76dd98881d2
nutelllia <natalia.struharova@icloud.com>
2022-03-03 15:04:07 +0100

    Add files, script and application

---
+++ project1-script.sh
@@ -0,0 +1,260 @@
+#!/bin/sh
+
+#  Word
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/word1.csv &
+open -a "Microsoft Word" desktop/Files/lorem_ipsum.rtf
+wait
+osascript -e 'quit app "Microsoft Word"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/word2.csv &
+open -a "Microsoft Word" desktop/Files/lorem_ipsum.rtf
+wait
+osascript -e 'quit app "Microsoft Word"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/word3.csv &
+open -a "Microsoft Word" desktop/Files/lorem_ipsum.rtf
+wait
+osascript -e 'quit app "Microsoft Word"'
+
+wait
+sleep 20
+
+#  Writer
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/writer1.csv &
+open -a "LibreOffice" desktop/Files/lorem_ipsum.rtf
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/writer2.csv &
+open -a "LibreOffice" desktop/Files/lorem_ipsum.rtf
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/writer3.csv &
+open -a "LibreOffice" desktop/Files/lorem_ipsum.rtf
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+#  Excel .ods
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/excel-ods-1.csv &
+open -a "Microsoft Excel" desktop/Files/2015.ods
+wait
+osascript -e 'quit app "Microsoft Excel"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/excel-ods-2.csv &
+open -a "Microsoft Excel" desktop/Files/2015.ods
+wait
+osascript -e 'quit app "Microsoft Excel"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/excel-ods-3.csv &
+open -a "Microsoft Excel" desktop/Files/2015.ods
+wait
+osascript -e 'quit app "Microsoft Excel"'
+
+wait
+sleep 20
+
+#  Excel .xls
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/excel-xls-1.csv &
+open -a "Microsoft Excel" desktop/Files/2015.xls
+wait
+osascript -e 'quit app "Microsoft Excel"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/excel-xls-2.csv &
+open -a "Microsoft Excel" desktop/Files/2015.xls
+wait
+osascript -e 'quit app "Microsoft Excel"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/excel-xls-3.csv &
+open -a "Microsoft Excel" desktop/Files/2015.xls
+wait
+osascript -e 'quit app "Microsoft Excel"'
+
+wait
+sleep 20
+
+#  Calc .ods
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/calc-ods-1.csv &
+open -a "LibreOffice" desktop/Files/2015.ods
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/calc-ods-2.csv &
+open -a "LibreOffice" desktop/Files/2015.ods
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/calc-ods-3.csv &
+open -a "LibreOffice" desktop/Files/2015.ods
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+#  Calc .xls
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/calc-xls-1.csv &
+open -a "LibreOffice" desktop/Files/2015.xls
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/calc-xls-2.csv &
+open -a "LibreOffice" desktop/Files/2015.xls
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/calc-xls-3.csv &
+open -a "LibreOffice" desktop/Files/2015.xls
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+#  PowerPoint .pptx
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/powerpoint-pptx-1.csv &
+open -a "Microsoft PowerPoint" desktop/Files/presentation.pptx
+wait
+osascript -e 'quit app "Microsoft PowerPoint"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/powerpoint-pptx-2.csv &
+open -a "Microsoft PowerPoint" desktop/Files/presentation.pptx
+wait
+osascript -e 'quit app "Microsoft PowerPoint"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/powerpoint-pptx-3.csv &
+open -a "Microsoft PowerPoint" desktop/Files/presentation.pptx
+wait
+osascript -e 'quit app "Microsoft PowerPoint"'
+
+wait
+sleep 20
+
+#  PowerPoint .odp
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/powerpoint-odp-1.csv &
+open -a "Microsoft PowerPoint" desktop/Files/presentation.odp
+wait
+osascript -e 'quit app "Microsoft PowerPoint"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/powerpoint-odp-2.csv &
+open -a "Microsoft PowerPoint" desktop/Files/presentation.odp
+wait
+osascript -e 'quit app "Microsoft PowerPoint"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/powerpoint-odp-3.csv &
+open -a "Microsoft PowerPoint" desktop/Files/presentation.odp
+wait
+osascript -e 'quit app "Microsoft PowerPoint"'
+
+wait
+sleep 20
+
+#  Impress .pptx
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/impress-pptx-1.csv &
+open -a "LibreOffice" desktop/Files/presentation.pptx
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/impress-pptx-2.csv &
+open -a "LibreOffice" desktop/Files/presentation.pptx
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/impress-pptx-3.csv &
+open -a "LibreOffice" desktop/Files/presentation.pptx
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+#  Impress .odp
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/impress-odp1.csv &
+open -a "LibreOffice" desktop/Files/presentation.odp
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/impress-odp2.csv &
+open -a "LibreOffice" desktop/Files/presentation.odp
+wait
+osascript -e 'quit app "LibreOffice"'
+
+wait
+sleep 20
+
+/Applications/Intel\ Power\ Gadget/PowerLog -duration 15 -file desktop/Results/impress-odp3.csv &
+open -a "LibreOffice" desktop/Files/presentation.odp
+wait
+osascript -e 'quit app "LibreOffice"'
+
+echo "Done!"
